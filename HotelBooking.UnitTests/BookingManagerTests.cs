a84f8a085907d527a4e5d271e23759e2e6d32371
denisraslavsky <denis.raslavsky@gmail.com>
2021-09-15 11:59:01 +0200

    Update BookingManagerTests.cs
    GetFullyOccupiedDates Tests

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
 using Xunit;
@@ -39,6 +40,43 @@ namespace HotelBooking.UnitTests
             int roomId = bookingManager.FindAvailableRoom(date, date);
             // Assert
             Assert.NotEqual(-1, roomId);
+        }
+
+        [Fact]
+        public void GetFullyOccupiedDates_StartDateIsMoreThanEndDate_ThrowArgumentException()
+        {
+            //Arrange
+            DateTime startDate = DateTime.Today.AddDays(5);
+            DateTime endDate = DateTime.Today.AddDays(2);
+            Action act = () => bookingManager.GetFullyOccupiedDates(startDate, endDate);
+            //Act
+            var rec = Record.Exception(act);
+            //Assert
+            Assert.IsType<ArgumentException>(rec);
+        }
+
+        [Fact]
+        public void GetFullyOccupiedDates_StartDayAdd21EndDateAdd25_ReturnsEmptyList()
+        {
+            //Arrange
+            DateTime startDate = DateTime.Today.AddDays(21);
+            DateTime endDate = DateTime.Today.AddDays(25);
+            //Act
+            List<DateTime> fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
+            //Assert
+            Assert.Empty(fullyOccupiedDates);
+        }
+
+        [Fact]
+        public void GetFullyOccupiedDates_StartDayAdd10EndDateAdd20_ReturnsListCount10()
+        {
+            //Arrange
+            DateTime startDate = DateTime.Today.AddDays(10);
+            DateTime endDate = DateTime.Today.AddDays(20);
+            //Act
+            List<DateTime> fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
+            //Assert
+            Assert.Equal(11, fullyOccupiedDates.Count);
         }
 
     }
