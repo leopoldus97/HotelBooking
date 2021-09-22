ea560de5685900e3a1605def6ef5143cce835c79
Marek Stančík <marekst11@gmail.com>
2021-09-22 11:28:29 +0200

    Testing github actions

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -33,6 +33,8 @@ namespace HotelBooking.UnitTests
                 new Room { Id=2, Description="B" },
             };
 
+            Assert.Empty(rooms);
+
             DateTime fullyOccupiedStartDate = DateTime.Today.AddDays(10);
             DateTime fullyOccupiedEndDate = DateTime.Today.AddDays(20);
 
